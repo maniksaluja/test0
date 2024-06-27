7dcfeaf1c6f0182ed18ca36bbae137dc26160185
Alpha <124826956+Alpha-Like@users.noreply.github.com>
2024-06-27 11:37:04 +0530

    Update start.py

--- start.py
+++ start.py
@@ -3,6 +3,13 @@ import os
 
 # Func for purging session files.
 def st():
+    os.system("git pull")
+    os.system("clear")
+    if not "manik" in os.listdir():
+        os.system("python3 -m venv manik")
+    os.system("source manik/bin/activate")
+    os.system("clear")
+    os.system("screen")
     print("Purging session files...")
     dir = os.listdir()
     for x in dir:
