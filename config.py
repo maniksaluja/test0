ee0974d80fa205a3c3cd9d49a6ae51eb6737a84e
maniksaluja <69381906+maniksaluja@users.noreply.github.com>
2024-07-21 10:36:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,11 +2,11 @@ from os import getenv
 
 API_ID = int(getenv('API_ID', '29321225'))
 API_HASH = getenv('API_HASH', '9af2b4ce5315e0222d0b30eba7b0905d')
-BOT_TOKEN = getenv('BOT_TOKEN', '6563909840:AAGCj0Ii3BjqCr5auxoSZUJZxKISr6KYr4c')
-BOT_TOKEN_2 = getenv('BOT_TOKEN_2', '7208277760:AAHs87ic7Z1WDUyPmPANGUYBdQTXV-8gM_c')
+BOT_TOKEN = getenv('BOT_TOKEN', '7427686742:AAEN6SEp9MrDEZUuphUAaRt3sJZaaNC5V6Y')
+BOT_TOKEN_2 = getenv('BOT_TOKEN_2', '6867248619:AAH5iMHJrxGgTpVHGE7t9nK5VzAXqYhVmiA')
 
-SUDO_USERS = getenv('SUDO_USERS', '6604279354') # Example: '1234 6789'
-MONGO_DB_URI = getenv('MONGO_DB_URI', 'mongodb+srv://terasharingbot:Vy3hQFVITnJ4RswM@cluster0.vn1hama.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+SUDO_USERS = getenv('SUDO_USERS', '6604279354 6919722801') # Example: '1234 6789'
+MONGO_DB_URI = getenv('MONGO_DB_URI', 'mongodb+srv://ishitaroy657boobs:bUE7wTKcdDabJkVR@geektyper.gzrindn.mongodb.net/')
 
 DB_CHANNEL_ID = int(getenv('DB_CHANNEL_ID', '-1002119403779'))
 DB_CHANNEL_2_ID = int(getenv('DB_CHANNEL_2_ID', '7150728058'))
