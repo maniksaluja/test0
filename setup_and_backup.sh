5c5ced1dbbc702df1d8ca962629167b6f619d218
maniksaluja <69381906+maniksaluja@users.noreply.github.com>
2024-11-30 23:12:41 +0530

    Update setup_and_backup.sh

--- setup_and_backup.sh
+++ setup_and_backup.sh
@@ -5,8 +5,11 @@ install_mongodb() {
     if ! command -v mongo &> /dev/null
     then
         echo "MongoDB is not installed. Installing MongoDB..."
+        
+        wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+        echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+        
         sudo apt update -y
-        sudo apt upgrade -y
         sudo apt install -y mongodb-org
         sudo systemctl start mongod
         sudo systemctl enable mongod
