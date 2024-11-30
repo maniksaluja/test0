1fefca042301dd2652bdc405061b50a07e99e1e8
maniksaluja <69381906+maniksaluja@users.noreply.github.com>
2024-11-30 23:18:42 +0530

    Update setup_and_backup.sh

--- setup_and_backup.sh
+++ setup_and_backup.sh
@@ -6,8 +6,8 @@ install_mongodb() {
     then
         echo "MongoDB is not installed. Installing MongoDB..."
         
-        wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -
-        echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+        wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
+        echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
         
         sudo apt update -y
         sudo apt install -y mongodb-org
