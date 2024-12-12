9f400e0e7daf8eb67f4804625ca8755e9a419c8e
maniksaluja <69381906+maniksaluja@users.noreply.github.com>
2024-12-12 20:15:11 +0530

    Update auto_delete.py

--- Database/auto_delete.py
+++ Database/auto_delete.py
@@ -1,4 +1,5 @@
 from . import db
+from time import time
 
 db = db.auto_delete
 
@@ -15,3 +16,11 @@ async def get_all() -> list[int]:
     x = db.find()
     x = await x.to_list(length=None)
     return [y['user_id'] for y in x]
+
+async def cleanup_expired_data():
+    """
+    Cleanup expired records from the database that are no longer required.
+    """
+    expiration_time = int(time()) - AUTO_DELETE_TIME
+    result = await db.delete_many({'dic.timestamp': {'$lt': expiration_time}})
+    print(f"Deleted {result.deleted_count} expired records.")
