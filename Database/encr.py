eb417d258b2140e54ec475fed5c73a65aca41b14
maniksaluja <69381906+maniksaluja@users.noreply.github.com>
2024-12-12 20:31:20 +0530

    Update encr.py

--- Database/encr.py
+++ Database/encr.py
@@ -3,9 +3,17 @@ from . import db
 db = db.encr
 
 async def update(encr, e):
-    await db.update_one({"encr": encr}, {"$set": {"e": e}}, upsert=True)
-    
+    try:
+        await db.update_one({"encr": encr}, {"$set": {"e": e}}, upsert=True)
+    except Exception as ex:
+        print(f"Error while updating encryption data: {ex}")
+
 async def get_encr(encr):
-    x = await db.find_one({"encr": encr})
-    if x:
-        return x["e"]
+    try:
+        x = await db.find_one({"encr": encr})
+        if x:
+            return x["e"]
+        return None  # Return None if no data found
+    except Exception as ex:
+        print(f"Error while retrieving encryption data: {ex}")
+        return None
