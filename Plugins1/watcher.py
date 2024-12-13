766205d1de6e2f923a9c7d841dc68aa9ca08efd9
maniksaluja <69381906+maniksaluja@users.noreply.github.com>
2024-12-13 16:43:27 +0530

    Update watcher.py

--- Plugins1/watcher.py
+++ Plugins1/watcher.py
@@ -8,5 +8,5 @@ from Database.users import add_user_2
 async def cwf(_, m):
     if m.from_user and m.from_user.id in SUDO_USERS:
         return await add_user_2(m.from_user.id)
-    await m.reply("**Message Me Here @CuteGirlTG**")
+    await m.reply("**Have Any Queries? @CuteGirlTG**")
     await add_user_2(m.from_user.id)
