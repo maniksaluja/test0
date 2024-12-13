e22e2abf722e04a669cb46c8e252e0c580ed5510
maniksaluja <69381906+maniksaluja@users.noreply.github.com>
2024-12-13 11:47:39 +0530

    Update batch.py

--- Plugins/batch.py
+++ Plugins/batch.py
@@ -24,67 +24,67 @@ TASK = bkl
 def get_TASK():
     return TASK.done()
 
-async def get_me(_):
+async def get_me(client):
     global me
     if not me:
-        me = await _.get_me()
+        me = await client.get_me()
     return me
 
 def in_batch(user_id):
     return user_id in dic or not get_TASK()
 
-async def send_delayed_message(chat_id, text, reply_markup=None, delay=3):
+async def send_delayed_message(client, chat_id, text, reply_markup=None, delay=3):
     """Send message with delay to avoid rate limiting issues."""
     await asyncio.sleep(delay)  # Delay to handle rate limit
-    return await sendMessage(chat_id, text, reply_markup)  # Send message using your existing sendMessage function
+    return await client.send_message(chat_id, text, reply_markup=reply_markup)
 
 @Client.on_message(filters.command('b') & filters.user(SUDO_USERS) & filters.private)
-async def batch(_, m):
-    if m.from_user.id in dic:
-        return await m.reply('**Batch UnderProcess Use /cancel For Stop!!.** ')
+async def batch(client, message):
+    if message.from_user.id in dic:
+        return await message.reply('**Batch Under Process Use /cancel For Stop!!**')
     if TASK and not TASK.done():
-        return await m.reply('Wait Until The Batch Gets Done.')
-    dic[m.from_user.id] = []
-    await m.reply('**OKAY Now I Can Make Batch Link When You Done Use /end **', quote=True)
+        return await message.reply('Wait Until The Batch Gets Done.')
+    dic[message.from_user.id] = []
+    await message.reply('**OKAY Now I Can Make Batch Link When You Are Done Use /end **', quote=True)
 
-async def batch_cwf(_, m):
-    if m.text:
-        if m.text.startswith('/'):
+async def batch_cwf(client, message):
+    if message.text:
+        if message.text.startswith('/'):
             return
-    if m.from_user.id in dic:
-        dic[m.from_user.id].append(m)
+    if message.from_user.id in dic:
+        dic[message.from_user.id].append(message)
 
 @Client.on_message(filters.command('cancel') & filters.user(SUDO_USERS) & filters.private)
-async def cancel(_, m):
-    if not m.from_user.id in dic:
-        return await m.reply('Nothing to cancel.')
-    dic.pop(m.from_user.id)
-    await m.reply('Batch Cancelled.')
+async def cancel(client, message):
+    if not message.from_user.id in dic:
+        return await message.reply('Nothing to cancel.')
+    dic.pop(message.from_user.id)
+    await message.reply('Batch Cancelled.')
 
-async def end(_, m):
-    if not m.from_user.id in dic:
+async def end(client, message):
+    if not message.from_user.id in dic:
         return
-    ms = dic[m.from_user.id]
-    dic.pop(m.from_user.id)
-    if not ms:
+    messages = dic[message.from_user.id]
+    dic.pop(message.from_user.id)
+    if not messages:
         return
-    iffff = await m.reply("**It Takes Few Minutes..**")
+    progress_message = await message.reply("**It Takes Few Minutes...**")
     dest_ids = []
     dest_ids_2 = []
     all_vid = True
-    for x in ms:
-        if not x.video:
+    for msg in messages:
+        if not msg.video:
             all_vid = False
-        new = await tryer(x.copy, DB_CHANNEL_ID, caption="#batch")
+        new = await tryer(msg.copy, DB_CHANNEL_ID, caption="#batch")
         dest_ids.append(new.id)
-        new = await tryer(x.copy, DB_CHANNEL_2_ID, caption="#batch")
+        new = await tryer(msg.copy, DB_CHANNEL_2_ID, caption="#batch")
         dest_ids_2.append(new.id)
         
         # Add delay to prevent flood
-        await send_delayed_message(m.chat.id, "Sending message... Please wait.")
+        await send_delayed_message(client, message.chat.id, "Sending message... Please wait.")
     
     if all_vid:
-        duration = sum([x.video.duration for x in ms])
+        duration = sum([msg.video.duration for msg in messages])
         duration = "⋞⋮⋟ " + alpha_grt(duration)
     else:
         duration = ''
@@ -94,23 +94,23 @@ async def end(_, m):
     encr_2 = encrypt(f'{Int2Char(dest_ids[0])}-{Int2Char(dest_ids[-1])}|{Int2Char(cur)}')
     await update(encr, encr_2)
     
-    link = f'https://t.me/{(await get_me(_)).username}?start=batchone{encr}'
+    link = f'https://t.me/{(await get_me(client)).username}?start=batchone{encr}'
     txt = LINK_GEN.format(f'{cur}', duration, link)
     markup = IKM([[IKB('Share', url=link)]])
     settings = await get_settings()
-    await iffff.delete()
+    await progress_message.delete()
     
     if LINK_GENERATE_IMAGE and settings['image']:
-        await m.reply_photo(LINK_GENERATE_IMAGE, caption=txt, reply_markup=markup, quote=True)
+        await message.reply_photo(LINK_GENERATE_IMAGE, caption=txt, reply_markup=markup, quote=True)
         if LOG_CHANNEL_ID and settings.get('logs', True):
-            await _.send_photo(LOG_CHANNEL_ID, LINK_GENERATE_IMAGE, caption=txt, reply_markup=markup)
+            await client.send_photo(LOG_CHANNEL_ID, LINK_GENERATE_IMAGE, caption=txt, reply_markup=markup)
     else:
-        await m.reply(txt, reply_markup=markup, quote=True)
+        await message.reply(txt, reply_markup=markup, quote=True)
         if LOG_CHANNEL_ID and settings.get('logs', True):
-            await _.send_message(LOG_CHANNEL_ID, txt, reply_markup=markup)
+            await client.send_message(LOG_CHANNEL_ID, txt, reply_markup=markup)
 
 @Client.on_message(filters.command('end') & filters.user(SUDO_USERS) & filters.private)
-async def endddd(_, m):
+async def endddd(client, message):
     global TASK
-    TASK = asyncio.create_task(end(_, m))
+    TASK = asyncio.create_task(end(client, message))
     await TASK
