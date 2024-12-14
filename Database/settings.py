f4609063dc9f98688718faacf49e54dcee52d2bf
maniksaluja <69381906+maniksaluja@users.noreply.github.com>
2024-12-14 19:48:13 +0530

    Update settings.py

--- Database/settings.py
+++ Database/settings.py
@@ -2,8 +2,7 @@ from . import db
 
 db = db.settings
 
-# purane settings ko touch nahi kiya gaya
-# blur ko new settings mein add kiya gaya hai
+# dic = {'auto_approval': False, 'join': False, 'leave': False, 'image': False}
 
 async def update_settings(dic):
     await db.update_one({'settings': 69}, {'$set': {'actual_settings': dic}}, upsert=True)
@@ -12,14 +11,4 @@ async def get_settings():
     x = await db.find_one({'settings': 69})
     if x:
         return x['actual_settings']
-    return {
-        'auto_approval': False, 
-        'join': False, 
-        'leave': False, 
-        'image': False, 
-        'generate': 10, 
-        'auto_save': False, 
-        'logs': True, 
-        'blur_enabled': False,  # Blur setting added
-        'blur_auto': False      # Blur Auto setting added
-    }
+    return {'auto_approval': False, 'join': False, 'leave': False, 'image': False, 'generate': 10, 'auto_save': False, 'logs': True}
